e25b4d2e1406778a6d71b5c6dd0fa4c14575236a
michaelilao <michaelilao@live.com>
2024-06-24 23:43:28 -0400

    icons

--- web/deploy.sh
+++ web/deploy.sh
@@ -4,5 +4,5 @@ npm install
 npx vite build 
 pm2 stop app.mach9aero.com 
 pm2 delete app.mach9aero.com 
-pm2 serve dist 4002 --spa --name app.mach9aero.com 
+pm2 serve dist 4001 --spa --name app.mach9aero.com 
 pm2 save --force
