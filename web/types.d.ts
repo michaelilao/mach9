2225563584772f5fbc927afeb081de9fbcbc9253
michaelilao <michaelilao@live.com>
2024-06-26 07:34:50 -0400

    c

--- web/types.d.ts
+++ web/types.d.ts
@@ -1,5 +1,6 @@
 import React from "react";
 
+declare module "leaflet";
 declare global {
 	type FormEvent = React.FormEvent<HTMLFormElement>;
 	type MouseEvent = React.MouseEvent<HTMLButtonElement>;
